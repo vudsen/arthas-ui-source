6243142e622c7f51b2f37ac3fca5af81faa54278
icebing <xu2237803016@outlook.com>
2025-03-11 15:13:05 +0800

    ci: 添加构建以及插件验证工作流

--- arthasui-core/build.gradle.kts
+++ arthasui-core/build.gradle.kts
@@ -1,6 +1,3 @@
-import org.jetbrains.intellij.platform.gradle.IntelliJPlatformType
-import org.jetbrains.intellij.platform.gradle.models.ProductRelease
-
 sourceSets {
     main {
         java {
@@ -21,6 +18,7 @@ plugins {
 }
 
 dependencies {
+    testImplementation(kotlin("test"))
     implementation("com.google.code.gson:gson:2.11.0")
     intellijPlatform {
         intellijIdeaCommunity(providers.gradleProperty("platformVersion"))
@@ -35,6 +33,20 @@ dependencies {
 
 group = "io.github.vudsen.arthasui"
 
+intellijPlatform {
+    pluginConfiguration {
+        ideaVersion {
+            sinceBuild = "232"
+            untilBuild = "243.*"
+        }
+    }
+    pluginVerification {
+        ides {
+            recommended()
+        }
+    }
+}
+
 tasks {
     // Set the JVM compatibility versions
     withType<JavaCompile> {
@@ -43,11 +55,6 @@ tasks {
     }
     withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
         kotlinOptions.jvmTarget = "17"
-    }
-
-    patchPluginXml {
-        sinceBuild.set("232")
-        untilBuild.set("243.*")
     }
 
     signPlugin {
@@ -60,20 +67,4 @@ tasks {
         token.set(System.getenv("PUBLISH_TOKEN"))
     }
 
-    intellijPlatform {
-        // Do not run this task on your own pc! It will download all the IDE between these version.
-        pluginVerification {
-            ides {
-                ide(IntelliJPlatformType.IntellijIdeaUltimate, providers.gradleProperty("platformVersion").get())
-                local(file(System.getenv("IDE_PATH") ?: "/fake"))
-                recommended()
-                select {
-                    types = listOf(IntelliJPlatformType.IntellijIdeaUltimate)
-                    channels = listOf(ProductRelease.Channel.RELEASE)
-                    sinceBuild = "233"
-                    untilBuild = "243.*"
-                }
-            }
-        }
-    }
 }
