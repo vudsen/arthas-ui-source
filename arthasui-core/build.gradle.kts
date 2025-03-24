241320d9c63f13dea8a4bbb6e62bdcbe0c2bb10b
vudsen <xu2237803016@outlook.com>
2025-03-24 09:03:24 +0800

    ci: 仅验证最老的和最新的版本

--- arthasui-core/build.gradle.kts
+++ arthasui-core/build.gradle.kts
@@ -93,7 +93,11 @@ intellijPlatform {
 
     pluginVerification {
         ides {
-            recommended()
+            val productReleases = ProductReleasesValueSource().get()
+            val reducedProductReleases =
+                if (productReleases.size > 2) listOf(productReleases.first(), productReleases.last())
+                else productReleases
+            ides(reducedProductReleases)
         }
     }
 }
