4334171ba053d977aeb7c0ecb905c0ba2e8a84cb
vudsen <xu2237803016@outlook.com>
2025-05-13 18:06:47 +0800

    测试报错

--- arthasui-bridge-impl/src/main/kotlin/io/github/vudsen/arthasui/bridge/host/K8sHostMachine.kt
+++ arthasui-bridge-impl/src/main/kotlin/io/github/vudsen/arthasui/bridge/host/K8sHostMachine.kt
@@ -17,18 +17,9 @@ import io.kubernetes.client.openapi.apis.VersionApi
 import io.kubernetes.client.openapi.models.V1Namespace
 import io.kubernetes.client.openapi.models.V1Pod
 import io.kubernetes.client.util.Config
-import org.apache.commons.compress.archivers.tar.TarArchiveEntry
-import org.apache.commons.compress.archivers.tar.TarArchiveOutputStream
-import org.apache.commons.compress.compressors.gzip.GzipCompressorOutputStream
-import java.io.BufferedOutputStream
-import java.io.ByteArrayInputStream
-import java.io.ByteArrayOutputStream
 import java.io.CharArrayReader
-import java.io.File
 import java.nio.charset.StandardCharsets
-import java.nio.file.Files
 import java.util.concurrent.TimeUnit
-import kotlin.io.path.Path
 
 class K8sHostMachine(private val config: HostMachineConfig) : HostMachine {
 
@@ -76,75 +67,6 @@ class K8sHostMachine(private val config: HostMachineConfig) : HostMachine {
         return StandardInteractiveShell(createOriginalInteractiveShell(jvm, *command))
     }
 
-
-    private fun packageDirectory(localPath: String): ByteArray {
-        val out = ByteArrayOutputStream()
-        GzipCompressorOutputStream(BufferedOutputStream(out)).use { gzipOut ->
-            TarArchiveOutputStream(gzipOut).use { tarOut ->
-                tarOut.setLongFileMode(TarArchiveOutputStream.LONGFILE_POSIX);
-                val sourceDir = Path(localPath)
-                Files.walk(sourceDir).forEach { path ->
-                    val relativePath = sourceDir.relativize (path).toString().replace(File.separatorChar, '/');
-                    if (Files.isDirectory(path)) {
-                        // 目录也需要作为 entry 添加进去
-                        if (!relativePath.isEmpty()) {
-                            val entry = TarArchiveEntry(path.toFile(), "$relativePath/");
-                            tarOut.putArchiveEntry(entry);
-                            tarOut.closeArchiveEntry();
-                        }
-                    } else {
-                        val entry = TarArchiveEntry(path.toFile(), relativePath);
-                        entry.size = Files.size(path);
-                        tarOut.putArchiveEntry(entry);
-                        Files.copy(path, tarOut);
-                        tarOut.closeArchiveEntry();
-                    }
-                }
-                tarOut.finish();
-
-            }
-        }
-        // tarOut.getBytesWritten()
-        return out.toByteArray()
-    }
-
-    /**
-     * 上传本地文件夹
-     * @param localPath 本地文件路径
-     * @param destPath 目标文件夹路径
-     */
-    fun uploadDirectory(jvm: PodJvm, localPath: String, destPath: String): CommandExecuteResult {
-        val progressIndicator = getUserData(HostMachine.PROGRESS_INDICATOR)?.get()
-        progressIndicator ?.let {
-            it.pushState()
-            it.text = "Uploading $localPath"
-        }
-
-        try {
-            val process = createInteractiveShell0(jvm, "tar", "-xmf", "-", "-C", destPath)
-            val byteArray = packageDirectory(localPath)
-            val inputStream = ByteArrayInputStream(byteArray)
-            var len = 0
-            var uploaded = 0
-            val buf = ByteArray(1024 * 1024)
-
-            while (inputStream.read(buf).also { len = it } != -1) {
-                process.outputStream.write(buf, 0, len)
-                ProgressManager.checkCanceled()
-                uploaded += len
-                progressIndicator?.let {
-                    it.fraction = uploaded.toDouble() / byteArray.size
-                }
-            }
-
-            return CommandExecuteResult(
-                String(process.inputStream.readAllBytes(), StandardCharsets.UTF_8),
-                process.exitValue()
-            )
-        } finally {
-            progressIndicator?.popState()
-        }
-    }
 
     fun isPodExist(name: String, namespace: String, container: String?): Boolean {
         val api = CoreV1Api(apiClient)
