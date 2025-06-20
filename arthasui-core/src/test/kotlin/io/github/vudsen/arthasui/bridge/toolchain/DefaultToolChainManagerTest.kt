515a83717a91d245961feaaa19e2222068c9c916
vudsen <xu2237803016@outlook.com>
2025-06-20 10:07:47 +0800

    chore: 移除不用的测试方法

--- arthasui-core/src/test/kotlin/io/github/vudsen/arthasui/bridge/toolchain/DefaultToolChainManagerTest.kt
+++ arthasui-core/src/test/kotlin/io/github/vudsen/arthasui/bridge/toolchain/DefaultToolChainManagerTest.kt
@@ -13,26 +13,6 @@ import java.io.File
 
 class DefaultToolChainManagerTest :  BasePlatformTestCase() {
 
-    fun testTransferPackage() {
-        val template = BridgeTestUtil.createSshHostMachine(testRootDisposable) {
-            withExtraHost("api.github.com", "127.0.0.1")
-            withExtraHost("github.com", "127.0.0.1")
-        }
-
-        val local = BridgeTestUtil.createLocalHostMachine()
-        local.mkdirs(template.resolveDefaultDataDirectory())
-        val file = File("${local.getHostMachineConfig().dataDirectory}/${DefaultToolChainManager.DOWNLOAD_DIRECTORY}/0_0_jattach-linux-x64.tgz")
-        try {
-            val expectedFiles = BridgeTestUtil.createTestTarGzFile(file)
-            val manager = DefaultToolChainManager(template, local)
-            val toolChainHomePath = manager.getToolChainHomePath(ToolChain.JATTACH_BUNDLE)
-            Assert.assertEquals(expectedFiles, template.listFiles(toolChainHomePath))
-        } finally {
-            file.delete()
-        }
-
-    }
-
     /**
      * 测试离线安装
      */
